07dc7aad2af4f08e3ceb78194041e0f2f56889e7
Ricardo M. Alvarado <ricardoma97@gmail.com>
2019-08-19 23:23:40 -0500

    WIP: tarea2

--- src/Routes/HW2Route.js
+++ src/Routes/HW2Route.js
@@ -6,6 +6,20 @@ import React, {
 
 export default function HW1Route() {
 	return (
-		<h1>Tarea 2</h1>
+		<div>
+			<h1>Tarea 2</h1>
+			<h4>
+				Realizar un programa que pueda reconocer palabras de los siguientes lenguajes: <br/>
+				1) El lenguaje de todas las cadenas que constan de n ceros seguidos de n unos para cualquier n ≥  0.<br/>
+					&nbsp;&nbsp;{`{λ, 01, 0011, 000111, …}`}<br/>
+				2) El conjunto de cadenas formadas por el mismo número de ceros que de unos:<br/>
+					&nbsp;&nbsp;{`{λ, 01, 10, 0011, 0101, 1001, …}`}<br/>
+			</h4>
+
+			<input 
+				className="input-field"
+				type="number"
+			/>
+		</div>
 	);
 }
