4fbcf3ac139b2215923be15ae36ec90cbb54c8f7
Roberto Javier Huerta González <a01273164@sldid15-2.local>
2019-09-05 13:00:18 -0500

    WIP: HW6

--- src/Router.js
+++ src/Router.js
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import HW1Route from './Routes/HW1Route';
 import HW2Route from './Routes/HW2Route';
 import HW3Route from './Routes/HW3Route';
+import HW6Route from './Routes/HW6Route';
 
 const HW1 = () => <HW1Route />
 const HW2 = () => <HW2Route />
 const HW3 = () => <HW3Route />
+const HW6 = () => <HW6Route />
 
 export default function AppRouter () {
 	return (
@@ -24,12 +26,16 @@ export default function AppRouter () {
 						<li>
 							<Link to="/tarea3">Tarea 3</Link>
 						</li>
+						<li>
+							<Link to="/tarea6">Tarea 6</Link>
+						</li>
 					</ul>
 				</nav>
 
 				<Route path="/tarea1" component={HW1} />
 				<Route path="/tarea2" component={HW2} />
 				<Route path="/tarea3" component={HW3} />
+				<Route path="/tarea6" component={HW6} />
 			</div>
 		</Router>
 	);
---
+++ src/Routes/HW6Route.js
@@ -0,0 +1,16 @@
+import React, {
+	useState,
+	useEffect,
+} from 'react';
+// import Matrix from '../Components/Matrix';
+// import Gist from 'react-gist';
+
+export default function HW3Route() {
+	return (
+		<div>
+			<h1>Tarea 6</h1>
+			<h4>Implemente en código la construcción de subconjuntos, para convertir un AFN a su correspondiente AFD.</h4>
+		</div>
+
+	)
+}
